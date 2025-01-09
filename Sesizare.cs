8553b26ec2a471dd31d51e1d317c220715ed1777
Horatiu <horatiu.doru@student.upt.ro>
2025-01-09 16:36:15 +0200

    Revert "Am pus pe git"
    This reverts commit 4a9fc7a002a7bac66488c674c4c70159a63b5070.

--- Sesizare.cs
+++
@@ -1,9 +0,0 @@
-namespace Proiect;
-
-public class Sesizare
-{
-    public int Id {get;set;}
-    public string Detalii {get;set;}
-    public string Status {get;set;}
-    public int IdLocuinta {get;set;}
-}
